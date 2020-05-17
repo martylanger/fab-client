88cb937ce868297baf27e24d56e63c74e5cf1de2
martylanger <langer.marty@gmail.com>
2020-05-17 14:55:11 -0400

    Introduce Game component
    Add Game.vue component and Game.vue view
    Add game to router and App.vue

---
+++ src/views/Game.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <Game />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Game from "@/components/Game.vue";
+
+export default {
+  name: "Game",
+  components: {
+    Game
+  }
+};
+</script>
