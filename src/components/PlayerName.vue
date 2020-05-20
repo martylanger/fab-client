f1366063f618e2c9cfea0791082d4e1b4a6e8208
martylanger <langer.marty@gmail.com>
2020-05-20 10:47:14 -0400

    Trim PlayerName.vue

--- src/components/PlayerName.vue
+++ src/components/PlayerName.vue
@@ -1,41 +1,17 @@
 <template>
   <div class="player-name">
-    <!-- <div v-if="this.phase === 'pregame'"> -->
-      <li>
-        {{ player.name }}
-        <button @click="$emit('remove', player.id)">
-          X
-        </button>
-      </li>
-    <!-- </div>
-    <div v-if="this.props.phase === 'gamePlay'">
-      <ul>
-        <li>{{ this.props.player.name }}</li>
-        <li>{{ this.props.player.spices }}</li>
-        <li>{{ this.props.player.tradingCards }}</li>
-        <li>{{ this.props.player.pointCards }}</li>
-      </ul>
-    </div> -->
+    <li>
+      {{ player.name }}
+      <button @click="$emit('remove', player.id)">
+        X
+      </button>
+    </li>
   </div>
 </template>
 
 <script>
-// import TradingCard from "./TradingCard.vue"
-// import PointCard from "./PointCard.vue"
-// import { store } from "@/store/index.js"
-
-// let phase = this.props
-
 export default {
   name: "PlayerName",
-  // components: {
-  //   TradingCard, PointCard
-  // },
-  data () {
-    return {
-      // player: this.props.player,
-    }
-  },
   props: {
     phase: {
       type: String,
@@ -45,11 +21,6 @@ export default {
       type: Object,
       required: true
     }
-  // }
-  // computed: {
-  //   isPregame () {
-  //     return phase === "pregame"
-  //   }
   }
 }
 </script>
