ef281e180ad4e4f2a14cfa876c8d24990e98771d
martylanger <langer.marty@gmail.com>
2020-05-20 16:34:43 -0400

    Add computed value points to pointCardSpaces

--- src/components/PointCardSpace.vue
+++ src/components/PointCardSpace.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="col-2 point-card-space">
+    <h2>{{ this.points }}</h2>
     <h6>{{ this.pointCard }}</h6>
   </div>
 </template>
@@ -17,6 +18,11 @@ export default {
       type: Array,
       required: false
     }
+  },
+  computed: {
+    points: function () {
+      return this.pointCard.reduce((a,b) => a + b, 0)
+    }
   }
 }
 </script>
@@ -25,7 +31,7 @@ export default {
 .point-card-space {
   border: 5px solid #9C6EA4;
   border-radius: 10px;
-  height: auto;
+  height: 120px;
 }
 /* use $zindex to place fabrics on the card */
 </style>
